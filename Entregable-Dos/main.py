07f06d3de02b0059476fb2319227fd596c795391
ArmandoArV <armando.arredondo.valle@gmail.com>
2023-06-05 17:41:18 -0600

    Added f-strings

--- Entregable-Dos/main.py
+++ Entregable-Dos/main.py
@@ -5,10 +5,9 @@ from formatCode import format_code
 from getInputFile import get_input_files_strings
 
 if __name__ == "__main__":
+    input_files = get_input_files_strings()
 
-    input_files_strings = get_input_files_strings()
-
-    for filename, input_string in input_files_strings:
+    for filename, input_string in input_files:
         lexer = Lexer(input_string)
         tokens = lexer.tokenize()
 
@@ -16,27 +15,34 @@ if __name__ == "__main__":
         token_tuples = [(token.type, token.lexeme) for token in tokens]
 
         # Print the inputted text
-        print("Name of the file:", filename)
-        print("Outputted text from the txt:\n", input_string)
+        print(f"Processing file: {filename}")
+        print(f"Input text:\n{input_string}")
 
-        # Print pretty table
+        # Print lexer analysis
         print("Lexer analysis:")
         print_table(token_tuples)
+
         # Generate HTML table
         table_html = format_table(token_tuples)
-        # Generate inputted text
+
+        # Generate HTML representation of input code
         input_html = format_code(input_string)
 
-        # Write HTML of table and inputted text to file
-        with open(f"{filename}.html", "w") as output_file:
+        # Write HTML of table and input code to file
+        output_filename = f"{filename}.html"
+        with open(output_filename, "w") as output_file:
             # Add HTML head with link to external CSS file
             output_file.write(
-                f"<html><head><link rel='stylesheet' type='text/css' href='styles.css'></head><body><h1 id='fileName'>File analyzed: {filename}</h1><h2 id='madeby'>Armando Arredondo Valle</h2>"
+                f"<html><head><link rel='stylesheet' type='text/css' href='styles.css'></head><body>"
+                f"<h1 id='filename'>File Analyzed: {filename}</h1>"
+                f"<h2 id='author'>Author: Armando Arredondo Valle</h2>"
             )
 
-            # Write input HTML and table HTML to file
+            # Write HTML representation of input code and table to file
             output_file.write(input_html)
             output_file.write(table_html)
 
             # Close HTML body and file
             output_file.write("</body></html>")
+
+        print(f"HTML report generated: {output_filename}")
